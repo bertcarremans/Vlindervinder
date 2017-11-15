1cbab0d79c631d82fe48e641647a3c32859467b0
Bert Carremans <bertcarremans@Berts-MacBook-Pro.local>
2017-11-15 22:26:20 +0100

    First commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# vlindervinder
+Tool built with Python and convolutional neural networks to classifiy butterfly images
---
+++ flickr/config.py
@@ -0,0 +1,4 @@
+API_KEY = 'a269db2de679d8ff1186c3e385a9a30a'
+API_SECRET = '68d696140c83cfa8'
+
+IMG_FOLDER = '/Users/bertcarremans/Data Science/Projecten/Vlindervinder/flickr/img/'
